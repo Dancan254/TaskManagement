8c827d508dda2542a3d17e0405648e385065b64e
Dancan254 <dancanian25@gmail.com>
2024-09-01 12:25:54 -0700

    refactor

--- src/main/java/org/taskmanager/DataBaseConfig.java
+++ src/main/java/org/taskmanager/DataBaseConfig.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.taskmanager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/main/java/org/taskmanager/Task.java
+++ src/main/java/org/taskmanager/Task.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.taskmanager;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
