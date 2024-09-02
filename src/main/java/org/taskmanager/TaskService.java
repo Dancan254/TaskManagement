af21250bce71e1f7b9fac7f4b450d6e620baf564
Dancan254 <dancanian25@gmail.com>
2024-09-02 03:31:01 -0700

    Add methods to update and retrieve tasks in TaskService

--- src/main/java/org/taskmanager/TaskService.java
+++ src/main/java/org/taskmanager/TaskService.java
@@ -1,6 +1,8 @@
 package org.taskmanager;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskService {
     public void createTask(Task task) {
@@ -66,5 +68,36 @@ public class TaskService {
             throw new RuntimeException("Failed to update task", e);
 
         }
+    }
+
+    //method to map the resultSet to the task for querying
+    public Task mapResultSetToTask(ResultSet resultSet) throws SQLException{
+
+        Task task = new Task();
+        task.setId(resultSet.getLong("id"));
+        task.setTitle(resultSet.getString("title"));
+        task.setDescription(resultSet.getString("description"));
+        task.setStatus(Task.Status.valueOf(resultSet.getString("status")));
+        task.setCreated_date(resultSet.getTimestamp("created_date").toLocalDateTime());
+        task.setDueDate(resultSet.getTimestamp("due_date") != null ? resultSet.getTimestamp("due_date").toLocalDateTime() : null);
+        return task;
+    }
+
+    //get all the tasks
+    public List<Task>  getAllTasks(){
+        String query = "SELECT * FROM tasks";
+        List<Task> tasks = new ArrayList<>();
+
+        try(Connection connection = DataBaseConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet set = statement.executeQuery()){
+            while(set.next()){
+                Task task = mapResultSetToTask(set);
+                tasks.add(task);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return tasks;
     }
 }
